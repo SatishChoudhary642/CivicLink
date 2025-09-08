20090ce0d88f74a335776b939e35782ec118f5ee
Satish <satishchoudhary642@gmail.com>
2025-09-08 17:12:15 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve './

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,15 +1,12 @@
 'use client';
 
-import { issues } from '@/lib/data';
 import MapLoader from '@/components/map/MapLoader';
+import { issues } from '@/lib/data';
 
 export default function MapPage() {
-  // In a real app, you might fetch this data, but for now we import it.
-  const allIssues = issues;
-
   return (
-    <div className="h-[calc(100vh-3.5rem)] w-full">
-      <MapLoader issues={allIssues} />
+    <div className="h-[calc(100vh-3.5rem)]">
+      <MapLoader issues={issues} />
     </div>
   );
 }
